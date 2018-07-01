1a09ba10010a016c834a6a5ff42256f56a56865c
Sehaba Amine <sehaba.amine2705@gmail.com>
2018-07-01 23:38:10 +0100

    Comment the code of DiscriminantAnalysis

--- Classifiers/DiscriminantAnalysis.py
+++ Classifiers/DiscriminantAnalysis.py
@@ -30,7 +30,7 @@ X = pca.fit_transform(X)
 #Split data to train and test 
 X_train, X_test, Y_train, Y_test = train_test_split (X,Y,test_size=0.2)
 
-#Create a Support Vector Classifier instance 
+#Create a Quadratic Discriminant Analysis instance 
 classifier = QuadraticDiscriminantAnalysis()
 
 #Fit the classifier
