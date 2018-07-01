65b6934a236308f430f7f58e93edad570775ec71
Sehaba Amine <sehaba.amine2705@gmail.com>
2018-07-01 23:35:18 +0100

    Comment the code of LinearRegression.py

--- Classifiers/LogisticRegression.py
+++ Classifiers/LogisticRegression.py
@@ -29,7 +29,7 @@ X = pca.fit_transform(X)
 #Split data to train and test 
 X_train, X_test, Y_train, Y_test = train_test_split (X,Y,test_size=0.2)
 
-#Create a Support Vector Classifier instance 
+#Create a Logistic Regression instance 
 classifier = LogisticRegression(C=1.0, penalty='l1')
 #classifier = LogisticRegression(C=1.0, penalty='l2')
 
